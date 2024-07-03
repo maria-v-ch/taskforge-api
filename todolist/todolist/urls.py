980ec36a12290326ee7ac072cfad0da057021810
maria-v-ch <m.chuvtaeva@gmail.com>
2024-07-03 07:05:19 +0300

    amended path for todolist to proceed with include('todo_app.urls') and not views.base, excluded import views

--- todolist/todolist/urls.py
+++ todolist/todolist/urls.py
@@ -16,10 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from todolist_app import views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('todolist/', views.base),
+    path('todolist/', include('todolist_app.urls')),
 ]
