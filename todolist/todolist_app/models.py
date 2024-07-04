a53c79a88200eca79a92bde95db2648fcc4b9b8e
maria-v-ch <m.chuvtaeva@gmail.com>
2024-07-04 18:05:59 +0300

    amended models.py: 1. added choices field for completed 2. added name 3. added description added class Meta with verbose_name and verbose_name_plural

--- todolist/todolist_app/models.py
+++ todolist/todolist_app/models.py
@@ -2,8 +2,18 @@ from django.db import models
 
 
 class Task(models.Model):
-    title = models.CharField(max_length=200)
-    completed = models.BooleanField(default=False)
+    class Completed(models.TextChoices):
+        YES = 'COMPLETED'
+        NO = 'IN PROGRESS'
 
-    def __str__(self):
-        return self.title
+    name = models.CharField(max_length=150, verbose_name='what to do (here keep long story really short)')
+    description = models.TextField(null=True, blank=True, verbose_name='here get here into details if needed')
+    due_date = models.DateTimeField(null=True, blank=True, verbose_name='Due to')
+    completed = models.CharField(
+        choices=Completed.choices,
+        default=Completed.NO
+    )
+
+    class Meta:
+        verbose_name = 'TO-DO'
+        verbose_name_plural = 'TO-DOs'
