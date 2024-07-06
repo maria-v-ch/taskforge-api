b853f63d3dcd8f6806f2619c641979460f215fdd
maria-v-ch <m.chuvtaeva@gmail.com>
2024-07-06 11:55:46 +0300

    1) Added new fields (status (Draft and Published), author, tags) to model Task. 2) Added new model Comment 3) added new model Tag

--- todolist/todolist_app/models.py
+++ todolist/todolist_app/models.py
@@ -2,10 +2,17 @@ from django.db import models
 
 
 class Task(models.Model):
+    STATUS_DRAFT = 'DRAFT'
+    STATUS_PUBLISHED = 'PUBLISHED'
+
     class Completed(models.TextChoices):
         YES = 'COMPLETED'
         NO = 'IN PROGRESS'
 
+    STATUSES = (
+        (STATUS_DRAFT, 'draft'),
+        (STATUS_PUBLISHED, 'published'),
+    )
     name = models.CharField(max_length=150, default='no name', verbose_name='what to do (here keep long story really '
                                                                             'short)')
     description = models.TextField(null=True, blank=True, verbose_name='here get here into details if needed')
@@ -14,7 +21,26 @@ class Task(models.Model):
         choices=Completed.choices,
         default=Completed.NO
     )
+    status = models.CharField(choices=STATUSES, default=STATUS_DRAFT, verbose_name='status', max_length=10)
+    author = models.CharField(max_length=200, null=True, blank=True)
+    tags = models.ManyToManyField('Tag', 'tasks')
 
     class Meta:
         verbose_name = 'TO-DO'
         verbose_name_plural = 'TO-DOs'
+
+
+class Comment(models.Model):
+    task = models.ForeignKey(Task, on_delete=models.CASCADE, related_name='comments')
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Comment by {self.pk} on {self.created_at}'
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
