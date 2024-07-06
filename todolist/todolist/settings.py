8c556408f2cfb1e4c45519d084d57b76a28da75f
maria-v-ch <m.chuvtaeva@gmail.com>
2024-07-06 13:29:49 +0300

    1) moved manage.py to root 2) added .env.sample 3) amended settings.py considering .env variables

--- todolist/todolist/settings.py
+++ todolist/todolist/settings.py
@@ -14,22 +14,25 @@ from pathlib import Path
 from dotenv import load_dotenv
 import os
 
-load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+dot_env = os.path.join(BASE_DIR, '.env')
+load_dotenv(dotenv_path=dot_env)
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
+
+ENV_TYPE = os.getenv('ENV_TYPE', 'local')
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG')
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', default='*').split()
 
 # Application definition
 
@@ -52,6 +55,9 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+if DEBUG and ENV_TYPE in ('local',):
+    MIDDLEWARE.append('base.middlewares.DebuggerMiddleware')
 
 ROOT_URLCONF = 'todolist.urls'
 
@@ -77,15 +83,13 @@ WSGI_APPLICATION = 'todolist.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'todolist',
-        'USER': 'a1',
-        # 'HOST': 'localhost',
-        # 'PASSWORD': 'andtime',
-        # 'PORT': '5432',
-    },
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.getenv('DB_NAME'),
+            'USER': os.getenv('DB_USER'),
+        },
 }
 
 
