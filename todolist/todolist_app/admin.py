bb5a3d20ce08508f914a73596089301a3ed4420d
maria-v-ch <m.chuvtaeva@gmail.com>
2024-07-06 11:50:13 +0300

    added CommentInline(admin.TabularInline), @admin.register(Task) class TaskAdmin(admin.ModelAdmin), @admin.register(Comment) class CommentAdmin(admin.ModelAdmin), admin.site.register(Tag)

--- todolist/todolist_app/admin.py
+++ todolist/todolist_app/admin.py
@@ -1,7 +1,33 @@
 from django.contrib import admin
-from todolist_app import models
+from todolist_app.models import Task, Comment, Tag
+
+# TabularInline — отображает связанные объекты в табличном виде. Есть также StackedInline, который отображает их в
+# виде блоков.
 
 
-admin.site.register(models.Task)
+class CommentInline(admin.TabularInline):
+    model = Comment
+    # extra = 0
 
-# Register your models here.
+    def get_extra(self, request, obj=None, **kwargs):
+        if obj:
+            return 0  # For changing an item
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('name', 'due_date', 'author')
+    search_fields = ('name', 'description', 'author', 'description')
+    list_filter = ('due_date', 'author', 'status')
+
+    inlines = [
+        CommentInline,
+    ]
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    search_fields = ['task__name', 'task__description', 'task__author']
+
+
+admin.site.register(Tag)
