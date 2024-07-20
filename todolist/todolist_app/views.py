1dbdae6b829d400a37b9a548247f137986cce5c7
maria-v-ch <m.chuvtaeva@gmail.com>
2024-07-20 11:40:02 +0300

    1. added TaskViewSet to views.py 2. muted base function for request

--- todolist/todolist_app/views.py
+++ todolist/todolist_app/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+from rest_framework import viewsets
 from .models import Task
+from .serializers import TaskSerializer
 
 
-def base(request):
-    tasks = Task.objects.all()
-    return render(request, 'todolist_app/base.html', {'tasks': tasks})
+# def base(request):
+#     tasks = Task.objects.all()
+#     return render(request, 'todolist_app/base.html', {'tasks': tasks})
+
+
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
