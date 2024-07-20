32fb00cefd93bb6063cacd232ba8578d6d15b0e7
maria-v-ch <m.chuvtaeva@gmail.com>
2024-07-20 11:35:54 +0300

    altered description and name in model Task

--- todolist/todolist_app/models.py
+++ todolist/todolist_app/models.py
@@ -13,9 +13,8 @@ class Task(models.Model):
         (STATUS_DRAFT, 'draft'),
         (STATUS_PUBLISHED, 'published'),
     )
-    name = models.CharField(max_length=150, default='no name', verbose_name='what to do (here keep long story really '
-                                                                            'short)')
-    description = models.TextField(null=True, blank=True, verbose_name='here get here into details if needed')
+    name = models.CharField(max_length=150, default='no name', verbose_name='what to do?')
+    description = models.TextField(null=True, blank=True, verbose_name='get into details if needed ...')
     due_date = models.DateTimeField(null=True, blank=True, verbose_name='due to')
     completed = models.CharField(
         choices=Completed.choices,
