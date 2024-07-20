8356695fbb2a072529a9d888d8299133204f3c74
maria-v-ch <m.chuvtaeva@gmail.com>
2024-07-20 23:44:18 +0300

    added serializers for Comment and Tag models

--- todolist/todolist_app/serializers.py
+++ todolist/todolist_app/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Task
+from .models import Task, Comment, Tag
 
 
 class TaskSerializer(serializers.ModelSerializer):
@@ -7,3 +7,14 @@ class TaskSerializer(serializers.ModelSerializer):
         model = Task
         fields = '__all__'
 
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = '__all__'
+
+
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = '__all__'
