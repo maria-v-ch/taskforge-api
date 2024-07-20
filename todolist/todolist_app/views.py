02a962360061115d0ec99f87ae6c875522fd832c
maria-v-ch <m.chuvtaeva@gmail.com>
2024-07-20 23:49:36 +0300

    1. created classes for generics based views for comments 2. unmuted base function

--- todolist/todolist_app/views.py
+++ todolist/todolist_app/views.py
@@ -1,14 +1,45 @@
 from django.shortcuts import render
-from rest_framework import viewsets
-from .models import Task
-from .serializers import TaskSerializer
+from rest_framework import viewsets, generics
+from .models import Task, Comment, Tag
+from .serializers import TaskSerializer, CommentSerializer, TagSerializer
 
 
-# def base(request):
-#     tasks = Task.objects.all()
-#     return render(request, 'todolist_app/base.html', {'tasks': tasks})
+def base(request):
+    tasks = Task.objects.all()
+    return render(request, 'todolist_app/base.html', {'tasks': tasks})
 
 
 class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
+
+
+class CommentCreateView(generics.CreateAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+
+
+class CommentListView(generics.ListAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+
+
+class CommentRetrieveView(generics.RetrieveAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+
+
+class CommentUpdateView(generics.UpdateAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+
+
+class CommentDestroyView(generics.DestroyAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+
+
+class TagViewSet(viewsets.ModelViewSet):
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+
