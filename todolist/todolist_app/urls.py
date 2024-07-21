07e35ef4065403bcffe49fd0cb31ef8ccc7fee02
maria-v-ch <m.chuvtaeva@gmail.com>
2024-07-21 12:30:42 +0300

    altered urls for CRUD being one-for-many in api

--- todolist/todolist_app/urls.py
+++ todolist/todolist_app/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import (TaskViewSet, CommentCreateView, CommentListView, CommentRetrieveView, CommentUpdateView,
-                    CommentDestroyView, TagViewSet)
+from .views import (TaskViewSet, CommentListCreateAPIView, CommentRetrieveUpdateDestroyAPIView, TagViewSet)
 
 
 app_name = 'tasks'
@@ -13,9 +12,11 @@ router.register('tags', TagViewSet)
 
 urlpatterns = [
    path('', include(router.urls)),
-   path('comments/create/', CommentCreateView.as_view(), name='comment-create'),
-   path('comments/', CommentListView.as_view(), name='comment-list'),
-   path('comments/<int:pk>/', CommentRetrieveView.as_view(), name='comment-retrieve'),
-   path('comments/<int:pk>/update/', CommentUpdateView.as_view(), name='comment-update'),
-   path('comments/<int:pk>/delete/', CommentDestroyView.as_view(), name='comment-destroy'),
+   # path('comments/create/', CommentCreateAPIView.as_view()),
+   # path('comments/', CommentListAPIView.as_view()),
+   # path('comments/<int:pk>/', CommentRetrieveAPIView.as_view()),
+   # path('comments/<int:pk>/update/', CommentUpdateAPIView.as_view()),
+   # path('comments/<int:pk>/delete/', CommentDestroyAPIView.as_view()),
+   path('comments/', CommentListCreateAPIView.as_view()),
+   path('comments/<int:pk>/', CommentRetrieveUpdateDestroyAPIView.as_view()),
 ]
