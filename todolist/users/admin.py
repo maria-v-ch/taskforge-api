dc024d0d44157a1113a23f18a5ec1996356c6371
maria-v-ch <m.chuvtaeva@gmail.com>
2024-07-27 19:53:51 +0300

    added to admin.py of users app registering useradmin

--- todolist/users/admin.py
+++ todolist/users/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User
 
-# Register your models here.
+
+class ToDoUserAdmin(BaseUserAdmin):
+    model = User
+    list_display = ('email', 'is_staff', 'is_active',)
+    list_filter = ('email', 'is_staff', 'is_active',)
+    search_fields = ('email',)
+    ordering = ('email',)
+
+
+admin.site.register(User, ToDoUserAdmin)
+
+
